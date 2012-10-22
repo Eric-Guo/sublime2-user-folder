f60f123c7eed796e5f0fd09cb317aaf4409df297
Eric Guo <eric.guo@sandisk.com>
2012-10-23 00:47:38 +0800

    Minus enhance the detect URL logic

--- OpenUrlInBrowser.py
+++ OpenUrlInBrowser.py
@@ -5,7 +5,7 @@ class OpenUrlInBrowserCommand(sublime_plugin.TextCommand):
 		for region in self.view.sel():
 			if not region.empty():
 				s = self.view.substr(region)
-				if s.find('.') == -1:
-					s = 'http://google.com/search?q='+self.view.substr(region)
+				if s.find('.') == -1 and s.find('://') == -1 :
+					s = 'http://google.com/search?q='+s.replace(' ','+')
 				command = "C:\\Program Files\\Mozilla Firefox\\firefox.exe " + s
 				subprocess.Popen(command)
