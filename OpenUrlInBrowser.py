b68331a3be647277cf2c35ffcbfc03a90193bd53
Eric Guo <eric.guo@sandisk.com>
2012-10-23 00:39:14 +0800

    Add function Open Url in Browser

---
+++ OpenUrlInBrowser.py
@@ -0,0 +1,11 @@
+import sublime, sublime_plugin, os, subprocess
+
+class OpenUrlInBrowserCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			if not region.empty():
+				s = self.view.substr(region)
+				if s.find('.') == -1:
+					s = 'http://google.com/search?q='+self.view.substr(region)
+				command = "C:\\Program Files\\Mozilla Firefox\\firefox.exe " + s
+				subprocess.Popen(command)
